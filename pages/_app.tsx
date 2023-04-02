2feb174a895a820660debe9653de946eb6b2a8b8
Manohar <dev.manoharkumar@gmail.com>
2023-04-03 02:38:43 +0530

    Added mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import ApplicationHeader from '@/components/ApplicationHeader';
+import type { AppProps } from 'next/app';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <ApplicationHeader/>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/signup.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <div className="h-full w-full flex justify-center">
+      Signup
+    </div>
+  )
+}
