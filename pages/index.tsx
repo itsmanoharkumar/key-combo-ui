be51cbe901c6a2a12d6305c964ff8389f739c7c4
Manohar <dev.manoharkumar@gmail.com>
2023-04-03 00:43:04 +0530

    Added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,6 +118,9 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </>
   )
 }
