de08ad9655c802f2e19cafb355befe1e7c4f61b3
Manohar <dev.manoharkumar@gmail.com>
2023-04-04 05:12:42 +0530

    implemented cookie-cutter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,18 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { CookieConsent } from 'react-cookie-consent';
 import { Provider } from 'react-redux';
 
 export default function App({Component, ...rest}: AppProps) {
-  const {store, props} = wrapper.useWrappedStore(rest)
+  const {store, props} = wrapper.useWrappedStore(rest);
   const {pageProps} = props;
   return (
     <>
       <Provider store={store}>
-      <ApplicationHeader/>
-      <Component {...pageProps} />
+        <ApplicationHeader/>
+        <Component {...pageProps} />
+        <CookieConsent>This website uses cookies to enhance the user experience.</CookieConsent>
       </Provider>
     </>
   );
