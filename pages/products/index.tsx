48c3ce831d288392f6ce41725065a2cb1c07fbc5
Manohar <dev.manoharkumar@gmail.com>
2023-04-12 03:35:22 +0530

    I immediately regret this commit.

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,19 +1,20 @@
 import ProductCardBox from "@/components/ProductCardBox";
 import { API_ROUTES } from "@/helpers/constants";
 import fetcher from "@/service/service";
+import { Product } from "@/types/types";
 import { useRouter } from "next/router";
-import useSWR from "swr";
 
-export default function Index() {
-  const { data, error, isLoading } = useSWR(API_ROUTES.products, fetcher);
+export async function getStaticProps() {
+  const { data } = await fetcher(API_ROUTES.products);
+  return {
+    props: {
+      productList: data,
+    },
+  };
+}
+
+export default function Index({ productList }: { productList: Product[] }) {
   const router = useRouter();
-  const productList = data?.data;
-  if (error) {
-    return <div>Some error occurred</div>;
-  }
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
   return (
     <div>
       <ul className={"flex flex-wrap justify-center m-2"}>
