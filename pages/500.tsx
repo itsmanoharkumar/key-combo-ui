a2ec8937a924a983f702d68c2e86bed3ed8aa8dd
Manohar <dev.manoharkumar@gmail.com>
2023-04-14 04:59:08 +0530

    wip

--- pages/500.tsx
+++ pages/500.tsx
@@ -4,24 +4,23 @@ import Link from "next/link";
 
 const Custom500 = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="flex items-center flex-col">
+    <div className="min-h-screen bg-gray-100 p-4 md:p-10 md:flex md:items-center md:justify-center w-full">
+      <div className="flex items-start flex-col md:items-center">
         <Image
           src={error500}
           alt="500 Internal Server Error"
-          width={500}
-          className={"mb-2"}
+          className={"mb-2 md:w-1/2 lg:w-1/3 w-full"}
         />
-        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+        <h1 className="text-xl md:text-4xl font-bold text-gray-900 mb-2">
           500 - Internal Server Error
         </h1>
-        <p className="text-lg text-gray-900 mb-4">
+        <p className="text-md md:text-2xl text-gray-900 mb-4">
           Oops! Something went wrong on our server. We are working to fix the
           issue.
         </p>
         <Link
           href="/"
-          className="text-lg text-gray-900 underline hover:text-gray-600"
+          className="text-lg md:text-2xl text-gray-900 underline hover:text-gray-600"
         >
           Go back to home
         </Link>
