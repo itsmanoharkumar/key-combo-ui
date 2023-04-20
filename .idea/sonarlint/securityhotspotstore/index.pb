3d3416cb3ba59b76c3c1c4572d8755250d76c3d9
Manohar <dev.manoharkumar@gmail.com>
2023-04-20 23:24:16 +0530

    reformatted code

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+G
+pages/products/[id].tsx,a\0\a0c7d798bc6aa6ecdfe531b09ab18e740d12f705
+S
+#components/ShortcutCateroryCard.tsx,b\1\b103acc413d8660146253cb17635e352ec28f5c2
+?
+pages/index.tsx,b\3\b3357635c26d1b52053382ce7e8a8640d31fedc8
+4
+.env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+>
+next.config.js,8\1\811a6d5a2e04fadb44d8eec365f2fa8af5c4facf
+Q
+!components/molecules/KeyCombo.tsx,8\7\874ac267d6c434047109a15535a858b5327213ef
+>
+types/types.ts,3\5\353ee6ebceb4d469d913e7558b730312d92d5199
+K
+components/KeyComboItem.tsx,3\6\366eb95571ec2d25cea946d6a8bf83064fe8a100
--- components/atoms/ProductCardItem.tsx
+++ components/atoms/ProductCardItem.tsx
@@ -7,13 +7,15 @@ interface ProductListItemProps extends Product {
 }
 
 export default function ProductCardItem({
-                                          id,
-                                          attributes: { name, logo },
-                                          onClick
-                                        }: ProductListItemProps) {
+  id,
+  attributes: { name, logo },
+  onClick,
+}: ProductListItemProps) {
   let imageUrl = logo?.data?.attributes?.formats?.thumbnail?.url;
   if (imageUrl?.startsWith("/")) {
-    imageUrl = imageUrl.replace("/", process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/").replace("api/", "");
+    imageUrl = imageUrl
+      .replace("/", process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/")
+      .replace("api/", "");
   }
   return (
     <div
@@ -21,13 +23,17 @@ export default function ProductCardItem({
       p-2 flex-wrap transition-all duration-100   hover:text-black hover:cursor-pointer my-1 rounded border-[1px] border-gray-300 hover:border-gray-400 hover:bg-pastelYellow w-[46%] sm:w-[30%] md:w-[150px] h-[150px] mx-1`}
       onClick={() => onClick(id)}
     >
-      {imageUrl && <div className={"p-2 bg-gray-100 border-gray-500 border-[1px] mb-2 rounded-2xl flex justify-center"}>
-        {imageUrl && <Image src={imageUrl}
-               alt="test"
-               width={50}
-               height={50}
-        />}
-      </div>}
+      {imageUrl && (
+        <div
+          className={
+            "p-2 bg-gray-100 border-gray-500 border-[1px] mb-2 rounded-2xl flex justify-center"
+          }
+        >
+          {imageUrl && (
+            <Image src={imageUrl} alt="test" width={50} height={50} />
+          )}
+        </div>
+      )}
       <div>{name}</div>
     </div>
   );
