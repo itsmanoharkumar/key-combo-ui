43750193d9ecab0bf98a0e7b7494daa736e09529
Manohar <itsmanoharkumar@gmail.com>
2023-05-31 17:41:19 +0530

    That's just how I roll

--- components/molecules/SideNavList.tsx
+++ components/molecules/SideNavList.tsx
@@ -1,23 +1,16 @@
 import { KeyboardAlt } from "@mui/icons-material";
-import ArrowRight from "@mui/icons-material/ArrowRight";
 import Dns from "@mui/icons-material/Dns";
-import Home from "@mui/icons-material/Home";
-import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import People from "@mui/icons-material/People";
 import PermMedia from "@mui/icons-material/PermMedia";
 import Public from "@mui/icons-material/Public";
-import Settings from "@mui/icons-material/Settings";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
-import Tooltip from "@mui/material/Tooltip";
 import * as React from "react";
 
 const data = [
@@ -47,8 +40,8 @@ export default function SideNavList({ routeList, onNavClick }: any) {
     <Box sx={{ display: "flex", width: "100%" }}>
       <Paper elevation={0} sx={{ maxWidth: 256, width: "100%" }}>
         <FireNav component="nav" disablePadding>
-          <ListItemButton component="a" href="#customized-list">
-            <ListItemIcon sx={{ fontSize: 20 }}>
+          <ListItemButton>
+            <ListItemIcon sx={{ my: 4, fontSize: 20 }}>
               <KeyboardAlt color="primary" />
             </ListItemIcon>
             <ListItemText
@@ -62,121 +55,20 @@ export default function SideNavList({ routeList, onNavClick }: any) {
             />
           </ListItemButton>
           <Divider />
-          <ListItem component="div" disablePadding>
-            <ListItemButton sx={{ height: 56 }}>
-              <ListItemIcon>
-                <Home color="primary" />
-              </ListItemIcon>
+          {routeList?.map((item: any, index: number) => (
+            <ListItemButton key={index} sx={{ py: 2 }}>
               <ListItemText
-                primary="Home"
+                primary={item.title}
                 primaryTypographyProps={{
-                  color: "primary",
+                  fontSize: 14,
                   fontWeight: "medium",
-                  variant: "body2",
+                }}
+                onClick={() => {
+                  onNavClick(item.href);
                 }}
               />
             </ListItemButton>
-            <Tooltip title="Settings">
-              <IconButton
-                size="large"
-                sx={{
-                  "& svg": {
-                    color: "rgba(255,255,255,0.8)",
-                    transition: "0.2s",
-                    transform: "translateX(0) rotate(0)",
-                  },
-                  "&:hover, &:focus": {
-                    bgcolor: "unset",
-                    "& svg:first-of-type": {
-                      transform: "translateX(-4px) rotate(-20deg)",
-                    },
-                    "& svg:last-of-type": {
-                      right: 0,
-                      opacity: 1,
-                    },
-                  },
-                  "&:after": {
-                    content: '""',
-                    position: "absolute",
-                    height: "80%",
-                    display: "block",
-                    left: 0,
-                    width: "1px",
-                    bgcolor: "divider",
-                  },
-                }}
-              >
-                <Settings />
-                <ArrowRight
-                  sx={{ position: "absolute", right: 4, opacity: 0 }}
-                />
-              </IconButton>
-            </Tooltip>
-          </ListItem>
-          <Divider />
-          <Box
-            sx={{
-              bgcolor: open ? "rgba(71, 98, 130, 0.2)" : null,
-              pb: open ? 2 : 0,
-            }}
-          >
-            <ListItemButton
-              alignItems="flex-start"
-              onClick={() => setOpen(!open)}
-              sx={{
-                px: 3,
-                pt: 2.5,
-                pb: open ? 0 : 2.5,
-                "&:hover, &:focus": { "& svg": { opacity: open ? 1 : 0 } },
-              }}
-            >
-              <ListItemText
-                primary="Navigation"
-                primaryTypographyProps={{
-                  fontSize: 15,
-                  fontWeight: "medium",
-                  lineHeight: "20px",
-                  mb: "2px",
-                }}
-                secondary={
-                  "Keyboard Shortcuts, Commands, About, Connect With Us"
-                }
-                secondaryTypographyProps={{
-                  noWrap: true,
-                  fontSize: 12,
-                  lineHeight: "16px",
-                  color: open ? "rgba(0,0,0,0)" : "rgba(255,255,255,0.5)",
-                }}
-                sx={{ my: 0 }}
-              />
-              <KeyboardArrowDown
-                sx={{
-                  mr: -1,
-                  opacity: 0,
-                  transform: open ? "rotate(-180deg)" : "rotate(0)",
-                  transition: "0.2s",
-                }}
-              />
-            </ListItemButton>
-            {open &&
-              routeList?.map((item: any, index: number) => (
-                <ListItemButton
-                  key={index}
-                  sx={{ py: 0, minHeight: 32, color: "rgba(255,255,255,.8)" }}
-                >
-                  <ListItemText
-                    primary={item.title}
-                    primaryTypographyProps={{
-                      fontSize: 14,
-                      fontWeight: "medium",
-                    }}
-                    onClick={() => {
-                      onNavClick(item.href);
-                    }}
-                  />
-                </ListItemButton>
-              ))}
-          </Box>
+          ))}
         </FireNav>
       </Paper>
     </Box>
